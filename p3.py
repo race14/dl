42c11f5256bb755fdd1c381e7e660f24924d8421
race14 <149912853+race14@users.noreply.github.com>
2023-11-07 12:24:11 +0530

    Update p3.py

--- p3.py
+++ p3.py
@@ -1,6 +1,3 @@
-# Small Image Classification Using Convolutional Neural Network (CNN)
-
-# In[1]:
 
 
 import tensorflow as tf
@@ -9,26 +6,26 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-# In[2]:
+
 
 
 cifar10 = tf.keras.datasets.cifar10
 (x_train, y_train), (x_test,y_test) = cifar10.load_data()
 
 
-# In[3]:
+
 
 
 x_train.shape
 
 
-# In[4]:
+
 
 
 x_test.shape
 
 
-# In[10]:
+
 
 
 x_train = x_train / 255.0
@@ -37,13 +34,13 @@ y_train = y_train.reshape(-1,)
 y_test = y_test.reshape(-1,)
 
 
-# In[11]:
+
 
 
 classes = ["airplane","automobile","bird","cat","deer","dog","frog","horse","ship","truck"]
 
 
-# In[12]:
+
 
 
 def plot_sample(x, y, index):
@@ -52,15 +49,12 @@ def plot_sample(x, y, index):
     plt.xlabel(classes[y[index]])
 
 
-# <h4 style="color:purple">Now let us build a convolutional neural network to train our images</h4>
-
-# In[13]:
 
 
 plot_sample(x_train, y_train, 0)
 
 
-# In[12]:
+
 
 
 model = models.Sequential([
@@ -76,17 +70,16 @@ model = models.Sequential([
 ])
 
 
-# In[13]:
-
 
 model.compile(optimizer='adam',
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
-# In[14]:
+
+
 model.fit(x_train, y_train, epochs=5)
 
 
-# In[22]:
+
 
 
 test_acc, test_loss = model.evaluate(x_test, y_test)
@@ -94,38 +87,30 @@ print('accuracy is', test_loss)
 print('loss is ',test_acc)
 
 
-# In[16]:
 
 
 y_pred = model.predict(x_test)
 
 
-# In[17]:
+
 
 
 y_classes = [np.argmax(element) for element in y_pred]
 y_classes[:5]
 
 
-# In[18]:
-
 
 y_test[:5]
 
-
-# In[19]:
 
 
 plot_sample(x_test, y_test,3)
 
 
-# In[20]:
-
-
 classes[y_classes[3]]
 
 
-# In[ ]:
+
 
 
 
